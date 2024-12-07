f52ccb52a95ac4fa3bd862e939decc8023cb7299
airgrade <charlesmugendi2020@gmail.com>
2024-12-07 07:42:52 +0300

    changed to dynamicallu pick a file

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from compressmodule import compress, decompress
+from tkinter import filedialog #this allows you to open up any file in your computer
 
+def open_file():
+    '''This function will open a file in the compter and return the path of that file'''
+    filename = filedialog.askopenfilename(initialdir='/', title="Select a file ")
+    return filename
+    
 def compression(i:str, o:str):
     '''i => inputfile ,  o=>outputfile'''
     compress(i, o)
@@ -13,32 +19,14 @@ window = tk.Tk()
 window.title("Compression Engine")
 window.geometry("640x480")
 
-#CREATE ENTRY FIELDS FOR COMPRESS
-input_entry = tk.Entry(window)
-output_entry = tk.Entry(window)
-input_label = tk.Label(window, text="File to be compressed")
-output_label = tk.Label(window, text="Compressed file name")
-input_label.grid(row=0, column=0, padx=5, pady=5)
-input_entry.grid(row=0, column=1,padx=5, pady=5)
-output_label.grid(row=1, column=0,padx=5, pady=5)
-output_entry.grid(row=1, column=1,padx=5, pady=5)
 
-#CREATE ENTRY FIELDS FOR DECOMPRESS
-d_input_entry = tk.Entry(window)
-d_output_entry = tk.Entry(window)
-d_input_label = tk.Label(window, text="Load compressed file")
-d_output_label = tk.Label(window, text="Decompressed file name")
-d_input_label.grid(row=3, column=0, padx=5, pady=5)
-d_input_entry.grid(row=3, column=1,padx=5, pady=5)
-d_output_label.grid(row=4, column=0,padx=5, pady=5)
-d_output_entry.grid(row=4, column=1,padx=5, pady=5)
 
 #CREATE BUTTON FOR COMPRESS
-compress_button = tk.Button(window, text="compress", command=lambda:compression(input_entry.get(), output_entry.get()))
-compress_button.grid(row=2, column=1, padx=5, pady=5)
+compress_button = tk.Button(window, text="compress", command=lambda:compression(open_file(), "compressedoutput1.txt"))
+compress_button.pack()
 
 #CREATE BUTTON FOR DECOMPRESS
-decompress_button = tk.Button(window, text="Decompress", command= lambda: decompression(d_input_entry.get(), d_output_entry.get()))
-decompress_button.grid(row=5, column=1)
+decompress_button = tk.Button(window, text="Decompress", command= lambda: decompression(open_file(), "decompressedout1"))
+decompress_button.pack()
 
 window.mainloop() 
